b31a6b3b73152384d658cc0a3bd4c2f2b6418aac
Artem Yunin <im.aryunin@gmail.com>
2022-10-21 20:39:17 +0300

    GSS: Added invalid bounds exception

--- GSS.cpp
+++ GSS.cpp
@@ -1,12 +1,16 @@
 #include "GSS.h"
 #include <cmath>
+#include <stdexcept>
 
 double GSS(double (*func)(double x), double a, double b, const double ERROR, int& iterCounter) {
+    if(func(a) * func(b) > 0) throw std::runtime_error("Invalid bounds");
+
     while(std::abs(b-a) > ERROR) {
         double c = a + 0.618*(b-a);
         if(func(a) * func(c) <= 0) b = c;
         else a = c;
         iterCounter++;
     }
+
     return a;
 }
